214b10edfa6e381e51f3c1caba71466f046541f4
Matheus Arleson Sales Xavier <matheusarleson@gmail.com>
2016-01-07 20:01:24 -0300

    Start to write javadoc.

--- src/main/java/br/com/xavier/crypto/pbe/PBECrypto.java
+++ src/main/java/br/com/xavier/crypto/pbe/PBECrypto.java
@@ -18,42 +18,88 @@ import javax.crypto.spec.SecretKeySpec;
 
 import br.com.xavier.crypto.KeySize;
 
+/**
+ * 
+ * Class to peform Password Based Encryption (PBE)
+ * 
+ * @author Matheus Arleson Sales Xavier
+ *
+ */
 public class PBECrypto {
 	
-	//XXX PROPERTIES
+	//XXX STATIC PROPERTIES
+	//CRYTO PROPERTIES
+	//TODO pass those as parameters?
+	private static final String cipherTransformation = "AES/CBC/PKCS5Padding";
+	private static final String keyFactoryAlgorithm = "PBKDF2WithHmacSHA256";
+	private static final String formatKeyAlgorithm = "AES";
+	
+	//DEFAULT PROPERTIES
+	private static final Integer defaultNumberOfIteractions = 65536;
+	private static final KeySize defaultKeySize = KeySize.BITS_256;
+	private static final Charset defaultCharset = Charset.forName("UTF-8");
+	
+	//XXX INSTANCE PROPERTIES
 	private final Charset charset;
 	private final Integer numberOfIterationsForKeyGeneration;
 	private final KeySize keySize;
+	
+	//XXX CRYPTO PROPERTIES
 	private final Cipher cipher;
 	private final SecretKeyFactory keyFactory;
 	private final SecureRandom secureRandomSaltGenerator;
 
 	//XXX CONSTRUCTOR
-	public PBECrypto(int numberOfIterations, KeySize keySize, Charset charset) throws GeneralSecurityException {
-		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+	/**
+	 * 
+	 * Constructs a reusable object with the parameters passed. 
+	 * 
+	 * @param numberOfIterations is the number of iteration to generate the key. Defaults to 65536.
+	 * @param keySize is the size in bits of the generated key. Defaults to 256;
+	 * @param charset of the characters passed in the password. Defaults to UTF-8.
+	 * 
+	 * @throws GeneralSecurityException
+	 */
+	public PBECrypto(Integer numberOfIterations, KeySize keySize, Charset charset) throws GeneralSecurityException {
+		cipher = Cipher.getInstance(cipherTransformation);
+		keyFactory = SecretKeyFactory.getInstance(keyFactoryAlgorithm);
+		
+		//from java 8 and on secureRandom is now seeded properly...
 		secureRandomSaltGenerator = new SecureRandom();
 		
-		if(numberOfIterations < 1){
-			this.numberOfIterationsForKeyGeneration = 65536;
+		//number of iterations fallback handling
+		if(numberOfIterations == null || numberOfIterations < 1){
+			this.numberOfIterationsForKeyGeneration = defaultNumberOfIteractions;
 		} else {
 			this.numberOfIterationsForKeyGeneration = numberOfIterations;
 		}
-		
+
+		//keysize fallback handling
 		if(keySize == null){
-			this.keySize = KeySize.BITS_256;
+			this.keySize = defaultKeySize;
 		} else {
 			this.keySize = keySize;
 		}
 		
+		//charset fallback handling
 		if(charset == null){
-			this.charset = Charset.forName("UTF-8");
+			this.charset = defaultCharset;
 		} else {
 			this.charset = charset;
 		}
 	}
 
 	//XXX ENCRYPT METHODS
+	/**
+	 * 
+	 * Encrypt a password returning a {@link PBEStorage} instance. 
+	 * 
+	 * @param password is the password to be encrypted.
+	 * @return {@link PBEStorage} instance holding the encrypted data.
+	 * @throws GeneralSecurityException if an exception occurs. Generic excetion wrapping the real exception. 
+	 * 
+	 * @see {@link PBEStorage}
+	 */
 	public PBEStorage encrypt(char[] password) throws GeneralSecurityException {
 		SecretKey key = deriveKey(password);
 
@@ -66,28 +112,49 @@ public class PBECrypto {
 		return pbeStorage;
 	}
 	
-	private SecretKey deriveKey(char[] password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+	/**
+	 * 
+	 * Derive a {@link SecretKey} from the password, number of iterations and key size passed on constructor.
+	 * 
+	 * @param password used in the variation. 
+	 * <br> A random salt is generated using the internal {@link SecureRandom} instance.
+	 * <br> Bytes from password are extracted using the {@link Charset} passed on the constructor.
+	 * 
+	 * @return A {@link SecretKey} generated from a {@link PBEKeySpec} formated in AES.
+	 * @throws GeneralSecurityException if an exception occurs. Generic excetion wrapping the real exception.
+	 */
+	private SecretKey deriveKey(char[] password) throws GeneralSecurityException {
 		byte[] salt = generateSalt();
-		
-		PBEKeySpec spec = new PBEKeySpec(password, salt, numberOfIterationsForKeyGeneration.intValue(), keySize.getKeySize());
-		SecretKey tmp = keyFactory.generateSecret(spec);
-		
-		spec.clearPassword();
-		
-		SecretKey key = new SecretKeySpec(tmp.getEncoded(), "AES");
-		return key;
+		PBEKeySpec spec = generatePBEKeySpec(password, salt, numberOfIterationsForKeyGeneration, keySize);
+		SecretKey rawKey = generateRawSecretKey(spec);
+		SecretKey secretKey = formatKey(rawKey, formatKeyAlgorithm);
+		return secretKey;
 	}
-
+	
 	private byte[] generateSalt() {
 		byte[] saltBytes = new byte[20];
 		secureRandomSaltGenerator.nextBytes(saltBytes);
 		return saltBytes;
 	}
 
+	private PBEKeySpec generatePBEKeySpec(char[] password, byte[] salt, int numberOfIterationsForKeyGeneration, KeySize keySize) {
+		PBEKeySpec PBEKeySpec = new PBEKeySpec(password, salt, numberOfIterationsForKeyGeneration, keySize.getKeySize());
+		return PBEKeySpec;
+	}
+	
+	private SecretKey generateRawSecretKey(PBEKeySpec spec) throws InvalidKeySpecException {
+		SecretKey rawSecretKey = keyFactory.generateSecret(spec);
+		spec.clearPassword();
+		return rawSecretKey;
+	}
+	
+	private SecretKeySpec formatKey(SecretKey tmp, String algorithm) {
+		return new SecretKeySpec(tmp.getEncoded(), algorithm);
+	}
+
 	//XXX DECRYPT METHODS
 	public char[] decrypt(PBEStorage pbeStorage) throws GeneralSecurityException {
 		IvParameterSpec ivParameterSpec = new IvParameterSpec(pbeStorage.getInitializationVector());
-		
 		cipher.init(Cipher.DECRYPT_MODE, pbeStorage.getKey(), ivParameterSpec);
 		byte[] decryptedContent = cipher.doFinal(pbeStorage.getCipherText());
 		return convertToCharArray(decryptedContent);
@@ -104,5 +171,18 @@ public class PBECrypto {
 		CharBuffer cb = CharBuffer.wrap(charArray);
 		ByteBuffer bb = charset.encode(cb);
 		return bb.array();
+	}
+	
+	//XXX GETTERS
+	public Charset getCharset() {
+		return charset;
+	}
+	
+	public Integer getNumberOfIterationsForKeyGeneration() {
+		return numberOfIterationsForKeyGeneration;
+	}
+	
+	public KeySize getKeySize() {
+		return keySize;
 	}
 }
