f3885efb0fbb8fb787665a4cc0effa2e448a9e46
Matheus Arleson Sales Xavier <matheusarleson@gmail.com>
2016-01-07 20:26:47 -0300

    More javadoc...

--- src/main/java/br/com/xavier/crypto/pbe/PBECrypto.java
+++ src/main/java/br/com/xavier/crypto/pbe/PBECrypto.java
@@ -30,14 +30,15 @@ public class PBECrypto {
 	//XXX STATIC PROPERTIES
 	//CRYTO PROPERTIES
 	//TODO pass those as parameters?
-	private static final String cipherTransformation = "AES/CBC/PKCS5Padding";
-	private static final String keyFactoryAlgorithm = "PBKDF2WithHmacSHA256";
-	private static final String formatKeyAlgorithm = "AES";
+	private static final String CIPHER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
+	private static final String KEY_FACTORY_ALGORITHM = "PBKDF2WithHmacSHA256";
+	private static final String FORMAT_KEY_ALGORITHM = "AES";
+	private static final int SALT_LENGTH = 20;
 	
 	//DEFAULT PROPERTIES
-	private static final Integer defaultNumberOfIteractions = 65536;
-	private static final KeySize defaultKeySize = KeySize.BITS_256;
-	private static final Charset defaultCharset = Charset.forName("UTF-8");
+	private static final Integer DEFAULT_NUMBER_OF_ITERACTIONS = 65536;
+	private static final KeySize DEFAULT_KEY_SIZE = KeySize.BITS_256;
+	private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 	
 	//XXX INSTANCE PROPERTIES
 	private final Charset charset;
@@ -61,29 +62,29 @@ public class PBECrypto {
 	 * @throws GeneralSecurityException
 	 */
 	public PBECrypto(Integer numberOfIterations, KeySize keySize, Charset charset) throws GeneralSecurityException {
-		cipher = Cipher.getInstance(cipherTransformation);
-		keyFactory = SecretKeyFactory.getInstance(keyFactoryAlgorithm);
+		cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+		keyFactory = SecretKeyFactory.getInstance(KEY_FACTORY_ALGORITHM);
 		
 		//from java 8 and on secureRandom is now seeded properly...
 		secureRandomSaltGenerator = new SecureRandom();
 		
 		//number of iterations fallback handling
 		if(numberOfIterations == null || numberOfIterations < 1){
-			this.numberOfIterationsForKeyGeneration = defaultNumberOfIteractions;
+			this.numberOfIterationsForKeyGeneration = DEFAULT_NUMBER_OF_ITERACTIONS;
 		} else {
 			this.numberOfIterationsForKeyGeneration = numberOfIterations;
 		}
 
 		//keysize fallback handling
 		if(keySize == null){
-			this.keySize = defaultKeySize;
+			this.keySize = DEFAULT_KEY_SIZE;
 		} else {
 			this.keySize = keySize;
 		}
 		
 		//charset fallback handling
 		if(charset == null){
-			this.charset = defaultCharset;
+			this.charset = DEFAULT_CHARSET;
 		} else {
 			this.charset = charset;
 		}
@@ -94,7 +95,7 @@ public class PBECrypto {
 	 * 
 	 * Encrypt a password returning a {@link PBEStorage} instance. 
 	 * 
-	 * @param password is the password to be encrypted.
+	 * @param password to be encrypted.
 	 * @return {@link PBEStorage} instance holding the encrypted data.
 	 * @throws GeneralSecurityException if an exception occurs. Generic excetion wrapping the real exception. 
 	 * 
@@ -127,29 +128,61 @@ public class PBECrypto {
 		byte[] salt = generateSalt();
 		PBEKeySpec spec = generatePBEKeySpec(password, salt, numberOfIterationsForKeyGeneration, keySize);
 		SecretKey rawKey = generateRawSecretKey(spec);
-		SecretKey secretKey = formatKey(rawKey, formatKeyAlgorithm);
+		SecretKey secretKey = formatKey(rawKey, FORMAT_KEY_ALGORITHM);
 		return secretKey;
 	}
 	
+	/**
+	 * 
+	 * Use the internal {@link SecureRandom} instance to generated a salt.
+	 * 
+	 * @return <b>byte[]</b> generated salt.
+	 */
 	private byte[] generateSalt() {
-		byte[] saltBytes = new byte[20];
+		byte[] saltBytes = new byte[SALT_LENGTH];
 		secureRandomSaltGenerator.nextBytes(saltBytes);
 		return saltBytes;
 	}
 
+	/**
+	 * 
+	 * Creates a {@link PBEKeySpec} from the parameters passed to be used to generate {@link SecretKey}.
+	 * 
+	 * @param password is the password being encrypted
+	 * @param salt is the random generated salt
+	 * @param numberOfIterationsForKeyGeneration is the number of iterations to hash before make a key
+	 * @param keySize is the number of bits the key will have
+	 * @return {@link PBEKeySpec} instance
+	 */
 	private PBEKeySpec generatePBEKeySpec(char[] password, byte[] salt, int numberOfIterationsForKeyGeneration, KeySize keySize) {
 		PBEKeySpec PBEKeySpec = new PBEKeySpec(password, salt, numberOfIterationsForKeyGeneration, keySize.getKeySize());
 		return PBEKeySpec;
 	}
 	
-	private SecretKey generateRawSecretKey(PBEKeySpec spec) throws InvalidKeySpecException {
+	/**
+	 * 
+	 * Creates a raw {@link SecretKey} using the {@link PBEKeySpec} passed.
+	 * 
+	 * @param spec is a {@link PBEKeySpec} that will generated the raw key
+	 * @return {@link SecretKey} generated from the spec
+	 * @throws GeneralSecurityException if an exception occurs. Generic excetion wrapping the real exception.
+	 */
+	private SecretKey generateRawSecretKey(PBEKeySpec spec) throws GeneralSecurityException {
 		SecretKey rawSecretKey = keyFactory.generateSecret(spec);
 		spec.clearPassword();
 		return rawSecretKey;
 	}
 	
-	private SecretKeySpec formatKey(SecretKey tmp, String algorithm) {
-		return new SecretKeySpec(tmp.getEncoded(), algorithm);
+	/**
+	 * 
+	 * Format a raw key using an algorithm.
+	 * 
+	 * @param rawKey is the rawKey to format.
+	 * @param algorithm is and cryptography algorithm. 
+	 * @return {@link SecretKeySpec} an formated key.
+	 */
+	private SecretKeySpec formatKey(SecretKey rawKey, String algorithm) {
+		return new SecretKeySpec(rawKey.getEncoded(), algorithm);
 	}
 
 	//XXX DECRYPT METHODS
